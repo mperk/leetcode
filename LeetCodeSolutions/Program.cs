2a8278b192aebe5ae2cdb4f8e6aef5d22f6f694e
Mehmet Perk <perkmehmed@gmail.com>
2022-06-29 19:21:38 +0300

    longest-palindromic-substring

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -23,27 +23,54 @@ using System.Numerics;
 
 //Console.WriteLine(FindMedianSortedArrays(new int[] {  }, new int[] {  }));
 
-Console.WriteLine(LongestPalindrome("babad"));
+Console.WriteLine(LongestPalindrome("cbbd"));
 
 Console.ReadLine();
 
 string LongestPalindrome(string s)
 {
-    //var allSubstrings = new List<string>();
+    //Manacher's Algorithm
     string longest = string.Empty;
+    int longLength = 0;
+    if (s.Length < 2) return s;
     for (int i = 0; i < s.Length; i++)
     {
-        for (int j = i; j < s.Length; j++)
+        int left = i;
+        int right = i;
+        extendPalindrome(s, ref left, ref right);
+        if(longLength < right - left - 1)
         {
-            string sub = s.Substring(i, j - i + 1);
-            if(sub == new string(sub.Reverse().ToArray()))
-            {
-                if (longest.Length < sub.Length)
-                    longest = sub;
-            }
+            longLength = right - left - 1;
+            longest = s.Substring(left+1, right - left - 1);
+        }
+
+        left = i;
+        right = i+1;
+        extendPalindrome(s, ref left, ref right);
+        if (longLength < right - left - 1)
+        {
+            longLength = right - left - 1;
+            longest = s.Substring(left + 1, right - left - 1);
         }
     }
+    if (longLength == 0) return s[0].ToString();
     return longest;
+}
+
+void extendPalindrome(string s, ref int left, ref int right)
+{
+    while (left >= 0 && right < s.Length)
+    {
+        if (s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        else
+        {
+            break;
+        }
+    }
 }
 
 double FindMedianSortedArrays(int[] nums1, int[] nums2)
---
+++ LeetCodeSolutions/longest-palindromic-substring.md
@@ -0,0 +1,49 @@
+﻿# longest-palindromic-substring
+**Solution**
+```
+string LongestPalindrome(string s)
+{
+    //Manacher's Algorithm
+    string longest = string.Empty;
+    int longLength = 0;
+    if (s.Length < 2) return s;
+    for (int i = 0; i < s.Length; i++)
+    {
+        int left = i;
+        int right = i;
+        extendPalindrome(s, ref left, ref right);
+        if(longLength < right - left - 1)
+        {
+            longLength = right - left - 1;
+            longest = s.Substring(left+1, right - left - 1);
+        }
+
+        left = i;
+        right = i+1;
+        extendPalindrome(s, ref left, ref right);
+        if (longLength < right - left - 1)
+        {
+            longLength = right - left - 1;
+            longest = s.Substring(left + 1, right - left - 1);
+        }
+    }
+    if (longLength == 0) return s[0].ToString();
+    return longest;
+}
+
+void extendPalindrome(string s, ref int left, ref int right)
+{
+    while (left >= 0 && right < s.Length)
+    {
+        if (s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+```
