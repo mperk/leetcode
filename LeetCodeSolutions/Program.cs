c14e1bed5f6312daa295dc1bb20a7329f3d045c6
Mehmet Perk <perkmehmed@gmail.com>
2022-07-19 08:51:42 +0300

    multiply-strings

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -81,8 +81,31 @@ using System.Text.Json;
 //CombinationSum(new int[] { 2, 3, 6, 7 }, 7);
 //CombinationSum2(new int[] { 10, 1, 2, 7, 6, 1, 5 }, 8);
 //Console.WriteLine(FirstMissingPositive(new int[] { -1, -2, -3 }));
-Console.WriteLine(Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+//Console.WriteLine(Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+Console.WriteLine(Multiply("123", "456"));
 Console.ReadLine();
+
+string Multiply(string num1, string num2)
+{
+    var result = new int[num1.Length + num2.Length];
+    for (int i = num1.Length - 1; i >= 0; i--)
+    {
+        for (int j = num2.Length - 1; j >= 0; j--)
+        {
+            int t = Int32.Parse(num1[i].ToString()) * Int32.Parse(num2[j].ToString()) + result[i + j + 1];
+            result[i + j] += t / 10;
+            result[i + j + 1] = t % 10;
+        }
+    }
+
+    var sb = new StringBuilder();
+    foreach (var item in result)
+    {
+        if (sb.Length == 0 && item == 0) continue;
+        else sb.Append(item.ToString());
+    }
+    return sb.Length == 0 ? "0" : sb.ToString();
+}
 
 int Trap(int[] height)
 {
---
+++ LeetCodeSolutions/multiply-strings.md
@@ -0,0 +1,26 @@
+﻿# multiply-strings
+
+**Solution**
+```
+string Multiply(string num1, string num2)
+{
+    var result = new int[num1.Length + num2.Length];
+    for (int i = num1.Length - 1; i >= 0; i--)
+    {
+        for (int j = num2.Length - 1; j >= 0; j--)
+        {
+            int t = Int32.Parse(num1[i].ToString()) * Int32.Parse(num2[j].ToString()) + result[i + j + 1];
+            result[i + j] += t / 10;
+            result[i + j + 1] = t % 10;
+        }
+    }
+
+    var sb = new StringBuilder();
+    foreach (var item in result)
+    {
+        if (sb.Length == 0 && item == 0) continue;
+        else sb.Append(item.ToString());
+    }
+    return sb.Length == 0 ? "0" : sb.ToString();
+}
+```
